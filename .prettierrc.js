7fea9cfb3150431ab917b6c7e1046afc01eb5239
kong <kong.hou@nine.com.au>
2022-03-01 20:21:49 +1100

    feat: init project

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: false,
+  tabWidth: 2,
+  printWidth: 80,
+  bracketSpacing: true,
+  bracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+};
